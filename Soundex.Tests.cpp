90dd1bbc05455669f62fbd3559ceaa576f996698
MAHAN <83449707+mahanthas@users.noreply.github.com>
2024-07-03 19:25:01 +0530

    Added Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,7 +1,6 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-
 TEST(SoundexTest, HandlesEmptyString) {
     EXPECT_EQ(generateSoundex(""), "");
 }
@@ -10,3 +9,42 @@ TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
+TEST(SoundexTest, HandlesSimpleNames) {
+    EXPECT_EQ(generateSoundex("Robert"), "R163");
+    EXPECT_EQ(generateSoundex("Rupert"), "R163");
+    EXPECT_EQ(generateSoundex("Rubin"), "R150");
+}
+
+TEST(SoundexTest, IgnoresNonAlphabeticCharacters) {
+    EXPECT_EQ(generateSoundex("O'Hara"), "O600");
+    EXPECT_EQ(generateSoundex("Smith!"), "S530");
+}
+
+TEST(SoundexTest, HandlesDoubleLetters) {
+    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
+}
+
+TEST(SoundexTest, HandlesNamesWithVowelsAndSilentLetters) {
+    EXPECT_EQ(generateSoundex("Pfister"), "P236");
+}
+
+TEST(SoundexTest, HandlesNamesWithHAndWSeparators) {
+    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
+    EXPECT_EQ(generateSoundex("Tymczak"), "T522");
+}
+
+TEST(SoundexTest, HandlesNamesWithRepeatedSoundexValues) {
+    EXPECT_EQ(generateSoundex("Gutierrez"), "G362");
+    EXPECT_EQ(generateSoundex("Jackson"), "J250");
+}
+
+TEST(SoundexTest, HandlesShortNames) {
+    EXPECT_EQ(generateSoundex("Li"), "L000");
+    EXPECT_EQ(generateSoundex("Lee"), "L000");
+}
+
+TEST(SoundexTest, PadsWithZerosWhenNeeded) {
+    EXPECT_EQ(generateSoundex("E"), "E000");
+    EXPECT_EQ(generateSoundex("Ei"), "E000");
+    EXPECT_EQ(generateSoundex("Eio"), "E000");
+}
